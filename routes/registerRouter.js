248b0f77a40cede9a2bf7b27bfb85de59b6d45d0
Rodrigo <digo.baiocchi@gmail.com>
2023-08-08 09:28:06 -0400

    added create table actions to Register router

--- routes/registerRouter.js
+++ routes/registerRouter.js
@@ -16,26 +16,51 @@ router.get('/', async (req, res, next) => {
     const createUsersTable = await query(createUsersTableQuery, (data) => {
         if(data) {
             console.log('Users Table has been creacted!');    
+        } else {
+            console.log('Users Table has already been created!');
         }
-        console.log('Users Table has already been created!');
     });
-    const createUserInfoTable = await query(createUserInfoTableQuery, () => {
-        console.log('Users Info Table has been creacted!');
+    const createUserInfoTable = await query(createUserInfoTableQuery, (data) => {
+        if(data) {
+            console.log('Users Info Table has been creacted!');    
+        } else {
+            console.log('Users Info Table has already been created!');
+        }
     });
-    const createCategoriesTable = await query(createCategoriesTableQuery, () => {
-        console.log('Users Categories Table has been creacted!');
+    const createCategoriesTable = await query(createCategoriesTableQuery, (data) => {
+        if(data) {
+            console.log('Categories Table has been creacted!');    
+        } else {
+            console.log('Categories Table has already been created!');
+        }
     });
-    const createProductsTable = await query(createProductsTableQuery, () => {
-        console.log('Users Products Table has been creacted!');
+    const createProductsTable = await query(createProductsTableQuery, (data) => {
+        if(data) {
+            console.log('Products Table has been creacted!');    
+        } else {
+            console.log('Products Table has already been created!');
+        }
     });
-    const createCategoryProductTable = await query(createCategoryProductTableQuery, () => {
-        console.log('Users Category_product Table has been creacted!');
+    const createCategoryProductTable = await query(createCategoryProductTableQuery, (data) => {
+        if(data) {
+            console.log('Category_product Table has been creacted!');    
+        } else {
+            console.log('Category_product Table has already been created!');
+        }
     });
-    const createCartTable = await query(createCartTableQuery, () => {
-        console.log('Users Cart Table has been creacted!');
+    const createCartTable = await query(createCartTableQuery, (data) => {
+        if(data) {
+            console.log('Cart Table has been creacted!');    
+        } else {
+            console.log('Cart Table has already been created!');
+        }
     });
-    const createPurchasingHistoryTable = await query(createPurchasingHistoryTableQuery, () => {
-        console.log('Users Purchasing History Table has been creacted!');
+    const createPurchasingHistoryTable = await query(createPurchasingHistoryTableQuery, (data) => {
+        if(data) {
+            console.log('Purchasing_History Table has been creacted!');    
+        } else {
+            console.log('Purchasing_History Table has already been created!');
+        }
     });
 
     const selectUsersTable = await query("SELECT * FROM users");
