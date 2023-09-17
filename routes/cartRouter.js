724653b835c4278e2c7dc404728b1c0c9a9aea7d
Rodrigo <digo.baiocchi@gmail.com>
2023-09-17 17:16:58 -0400

    deleted PUT action for cart router. Changes to cart are done by POST action

--- routes/cartRouter.js
+++ routes/cartRouter.js
@@ -120,30 +120,6 @@ router.get('/', async (req, res, next) => {
     return res.status(200).json({msg: `Cart selected for user ${userId}`, cart: userCartData});
 });
 
-router.put('/', async (req, res, next) => {
-    const { userId, productId, totalUnits } = req.body;
-
-    const validUserId = await Database.selectUserById(userId);
-    if(!validUserId) {
-        return res.status(400).json(`User id ${userId} was not found`);
-    }
-
-    const validProductId = await Database.checkIfProductAlreadyExists(productId);
-    if(!validProductId) {
-        return res.status(400).json(`Product id ${productId} was not found`);
-    }
-
-    const getProductData = await Database.getProductById(productId);
-    const totalProductQty = getProductData.total_available;
-    if(totalUnits < 1 || totalUnits > totalProductQty) {
-        return res.status(400).json(`Product has less than ${totalUnits} units`)
-    }
-
-    const updateCartProductQuantity = await Database.updateProductQuanityInCart(userId, productId, totalUnits)
-
-    return res.status(200).json(`Quantity for product id ${productId} was updated in the cart`);
-});
-
 router.delete('/:userId/:productId', async (req, res, next) => {
     const userId = Number(req.params.userId);
     const productId = Number(req.params.productId);
