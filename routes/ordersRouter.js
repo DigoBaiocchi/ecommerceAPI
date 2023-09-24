7d8fb2128d9c1f9295011a0c052b6673bf09c58d
Rodrigo <digo.baiocchi@gmail.com>
2023-09-24 18:51:07 -0400

    setting up orders routes and tests with no database connection

--- routes/ordersRouter.js
+++ routes/ordersRouter.js
@@ -6,4 +6,59 @@ router.get('/', (req, res, next) => {
     return res.status(200).json(`All orders are loaded`);
 });
 
+router.get('/:userId', (req, res, next) => {
+    const userId = Number(req.params.userId);
+    const validUser = userId === 6;
+
+    if(!validUser) {
+        return res.status(400).json(`User id ${userId} does not exists`)
+    }
+    return res.status(200).json(`All orders for user ${userId} were loaded`);
+});
+
+router.put('/:userId/:orderId', (req, res, next) => {
+    const userId = Number(req.params.userId);
+    const orderId = Number(req.params.orderId);
+
+    const validUser = userId === 6;
+    if(!validUser) {
+        return res.status(400).json(`User id ${userId} was not found`)
+    }
+
+    const validOrder = orderId === 1;
+    if(!validOrder) {
+        return res.status(400).json(`Order number ${orderId} was not found`)
+    }
+
+    return res.status(200).json(`Order ${orderId} has been updated for user ${userId}`);
+});
+
+router.delete('/:userId/:orderId', (req, res, next) => {
+    const userId = Number(req.params.userId);
+    const orderId = Number(req.params.orderId);
+
+    const validUser = userId === 6;
+    if(!validUser) {
+        return res.status(400).json(`User id ${userId} was not found`)
+    }
+
+    const validOrder = orderId === 1;
+    if(!validOrder) {
+        return res.status(400).json(`Order number ${orderId} was not found`)
+    }
+
+    return res.status(200).json(`Order number ${orderId} was deleted`);
+});
+
+router.delete('/:userId', (req, res, next) => {
+    const userId = Number(req.params.userId);
+
+    const validUser = userId === 6;
+    if(!validUser) {
+        return res.status(400).json(`User id ${userId} was not found`)
+    }
+
+    return res.status(200).json(`All user's ${userId} orders were deleted`);
+});
+
 module.exports = router;
--- tests/ordersApiTest.js
+++ tests/ordersApiTest.js
@@ -3,13 +3,170 @@ const app = require('../app');
 
 describe('GET /orders', () => {
     const path = '/orders'
-    it('Select all the orders from database', (done) => {
+    it('Responses with 200 when all orders are loaded from database', (done) => {
         request(app)
             .get(path)
             .set('accept', 'application/json')
             .expect('Content-Type', /json/)
             .expect(200)
             .expect(`"All orders are loaded"`)
+            .end((err) => {
+                if(err) return done(err);
+                done();
+            })
+    });
+});
+
+describe('GET /orders/:userId', () => {
+    const userId = 6;
+    const path = `/orders/${userId}`;
+    const badUserId = 1;
+    const badPath = `/orders/${badUserId}`;
+    it('Responses with 200 when all orders from user was loaded from database', (done) => {
+        request(app)
+            .get(path)
+            .set('accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .expect(`"All orders for user ${userId} were loaded"`)
+            .end((err) => {
+                if (err) return done(err);
+                done();
+            })
+    });
+    it('Responses with 400 user does not exists', (done) => {
+        request(app)
+            .get(badPath)
+            .set('accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(400)
+            .expect(`"User id ${badUserId} does not exists"`)
+            .end((err) => {
+                if (err) return done(err);
+                done();
+            })
+    });
+});
+
+describe('PUT /orders/userId/orderId', () => {
+    const userId = 6;
+    const orderId = 1;
+    const path = `/orders/${userId}/${orderId}`;
+    const badUserId = 1;
+    const badOrderId = 1000;
+    const badUserPath = `/orders/${badUserId}/${orderId}`;
+    const badOrderPath = `/orders/${userId}/${badOrderId}`;
+
+    it('responses with 200 when order has been updated', (done) => {
+        request(app)
+            .put(path)
+            .set('accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .expect(`"Order ${orderId} has been updated for user ${userId}"`)
+            .end((err) => {
+                if (err) return done(err);
+                done();
+            })
+    });
+    it('responses with 400 when user id was not found', (done) => {
+        request(app)
+            .put(badUserPath)
+            .set('accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(400)
+            .expect(`"User id ${badUserId} was not found"`)
+            .end((err) => {
+                if(err) return done(err);
+                done();
+            })
+    });
+    it('responses with 400 when order number was not found', (done) => {
+        request(app)
+            .put(badOrderPath)
+            .set('accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(400)
+            .expect(`"Order number ${badOrderId} was not found"`)
+            .end((err) => {
+                if (err) return done(err);
+                done();
+            })
+    });
+});
+
+describe('DELETE /orders/:userId/:orderId', () => {
+    const userId = 6;
+    const orderId = 1;
+    const path = `/orders/${userId}/${orderId}`;
+    const badUserId = 1;
+    const badOrderId = 1000;
+    const badUserPath = `/orders/${badUserId}/${orderId}`;
+    const badOrderPath = `/orders/${userId}/${badOrderId}`;
+
+    it('responses with 200 when order was deleted', (done) => {
+        request(app)
+            .delete(path)
+            .set('accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .expect(`"Order number ${orderId} was deleted"`)
+            .end((err) => {
+                if (err) return done(err);
+                done();
+            })
+    });
+    it('responses with 400 when user id was not found', (done) => {
+        request(app)
+            .delete(badUserPath)
+            .set('accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(400)
+            .expect(`"User id ${badUserId} was not found"`)
+            .end((err) => {
+                if(err) return done(err);
+                done();
+            })
+    });
+    it('responses with 400 when order number was not found', (done) => {
+        request(app)
+            .delete(badOrderPath)
+            .set('accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(400)
+            .expect(`"Order number ${badOrderId} was not found"`)
+            .end((err) => {
+                if (err) return done(err);
+                done();
+            })
+    });
+});
+
+describe('DELETE /orders/:userId', () => {
+    const userId = 6;
+    const path = `/orders/${userId}`;
+    const badUserId = 1;
+    const badUserPath = `/orders/${badUserId}`;
+
+    it('responses with 200 when all orders were delete from a user', (done) => {
+        request(app)
+            .delete(path)
+            .set('accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .expect(`"All user's ${userId} orders were deleted"`)
+            .end((err) => {
+                if (err) return done(err);
+                done();
+            })
+    });
+    it('responses with 400 when user id was not found', (done) => {
+        request(app)
+            .delete(badUserPath)
+            .set('accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(400)
+            .expect(`"User id ${badUserId} was not found"`)
             .end((err) => {
                 if(err) return done(err);
                 done();
