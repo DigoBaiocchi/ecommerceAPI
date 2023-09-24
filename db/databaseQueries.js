b079f76e5931bb6752ced34cd6206444d4379694
Rodrigo <digo.baiocchi@gmail.com>
2023-09-23 21:30:12 -0400

    fixing checkLastOrderNumber function

--- db/databaseQueries.js
+++ db/databaseQueries.js
@@ -203,7 +203,7 @@ const Database = {
         return await query(`DELETE FROM cart WHERE user_id = $1`, [userId]);
     },
     async checkLastOrderNumber() {
-        const listOfOrders = await query(`SELECT order_number FROM orders`).then(results = results.rows);
+        const listOfOrders = await query(`SELECT order_number FROM orders`).then(results => results.rows);
         if (listOfOrders.length === 0) {
             return 0;
         } else {
