617a0aa52e602fa45af638d37b619a5d71943829
Rodrigo <digo.baiocchi@gmail.com>
2023-09-25 19:57:02 -0400

    connecting order router actions to database

--- routes/ordersRouter.js
+++ routes/ordersRouter.js
@@ -2,61 +2,95 @@ const express = require('express');
 const router = express.Router();
 const { Database } = require('../db/databaseQueries');
 
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
+    const orders = await Database.getAllOrders();
     return res.status(200).json(`All orders are loaded`);
 });
 
-router.get('/:userId', (req, res, next) => {
+router.get('/:userId', async (req, res, next) => {
     const userId = Number(req.params.userId);
-    const validUser = userId === 6;
+    const validUser = await Database.selectUserById(userId);;
 
     if(!validUser) {
         return res.status(400).json(`User id ${userId} does not exists`)
     }
+
+    const userOrders = await Database.getAllUserOrders(userId);
     return res.status(200).json(`All orders for user ${userId} were loaded`);
 });
 
-router.put('/:userId/:orderId', (req, res, next) => {
+router.get('/:userId/:orderId', async (req, res, next) => {
     const userId = Number(req.params.userId);
     const orderId = Number(req.params.orderId);
 
-    const validUser = userId === 6;
+    const validUser = await Database.selectUserById(userId);
     if(!validUser) {
         return res.status(400).json(`User id ${userId} was not found`)
     }
 
-    const validOrder = orderId === 1;
-    if(!validOrder) {
+    const validOrder = await Database.getOrderData(userId, orderId);
+    console.log(validOrder)
+    if(validOrder.length === 0) {
         return res.status(400).json(`Order number ${orderId} was not found`)
     }
 
+    return res.status(200).json(`Order ${orderId} has been selected for user ${userId}`);
+});
+
+router.patch('/:userId/:orderId', async (req, res, next) => {
+    const userId = Number(req.params.userId);
+    const orderId = Number(req.params.orderId);
+    const { orderStatus } = req.body;
+
+    if(!orderStatus) {
+        return res.status(400).json(`No order status was provided`);
+    }
+
+    const validUser = await Database.selectUserById(userId);
+    if(!validUser) {
+        return res.status(400).json(`User id ${userId} was not found`)
+    }
+
+    const validOrder = await Database.getOrderData(userId, orderId);
+    
+    if(validOrder.length === 0) {
+        return res.status(400).json(`Order number ${orderId} was not found`)
+    }
+    
+    const updateOrderStatus = await Database.updateOrderStatus(orderId, userId, orderStatus);
+    
     return res.status(200).json(`Order ${orderId} has been updated for user ${userId}`);
 });
 
-router.delete('/:userId/:orderId', (req, res, next) => {
+router.delete('/:userId/:orderId', async (req, res, next) => {
     const userId = Number(req.params.userId);
     const orderId = Number(req.params.orderId);
 
-    const validUser = userId === 6;
+    const validUser = await Database.selectUserById(userId);
     if(!validUser) {
         return res.status(400).json(`User id ${userId} was not found`)
     }
 
-    const validOrder = orderId === 1;
-    if(!validOrder) {
+    const validOrder = await Database.getOrderData(userId, orderId);
+    
+    if(validOrder.length === 0) {
         return res.status(400).json(`Order number ${orderId} was not found`)
     }
+
+    const deleteOrder = await Database.deleteOrder(orderId, userId);
 
     return res.status(200).json(`Order number ${orderId} was deleted`);
 });
 
-router.delete('/:userId', (req, res, next) => {
+router.delete('/:userId', async (req, res, next) => {
     const userId = Number(req.params.userId);
 
-    const validUser = userId === 6;
+    const validUser = await Database.selectUserById(userId);
     if(!validUser) {
         return res.status(400).json(`User id ${userId} was not found`)
     }
+
+    const deleteAllUserOrders = await Database.deleteAllUserOrders(userId);
 
     return res.status(200).json(`All user's ${userId} orders were deleted`);
 });
