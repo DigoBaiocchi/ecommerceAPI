00de81c0a965f9791e2bf4aed871d102f933fbe6
Rodrigo <digo.baiocchi@gmail.com>
2023-12-17 17:31:57 -0500

    refactoring json error objects and instead of passing userId id via parameter, userId is passed vie query parameter in cartRouter

--- routes/cartRouter.js
+++ routes/cartRouter.js
@@ -70,7 +70,7 @@ const { Database } = require('../db/databaseQueries');
 router.post('/', async (req, res, next) => {
     // check if user is not logged in
     if(!req.session.passport) {
-        return res.status(500).json({msg: `User is not logged in`});    
+        return res.status(500).json({ error: `User is not logged in` });
     } else {
         const { productId, totalUnits } = req.body;
         const userId = req.session.passport.user.userId;
@@ -78,37 +78,37 @@ router.post('/', async (req, res, next) => {
         
         const validUserId = await Database.selectUserById(userId);
         if(!validUserId) {
-            return res.status(400).json(`User id was not found`);
+            return res.status(400).json({ error: `User id was not found` });
         }
         
         const validProductId = await Database.checkIfProductAlreadyExists(productId);
         if(!validProductId) {
-            return res.status(401).json(`Product id was not found`);
+            return res.status(401).json({ error: `Product id was not found` });
         }
         
         const getProductData = await Database.getProductById(productId);
         const totalProductQty = getProductData.total_available;
         if(totalUnits > totalProductQty) {
-            return res.status(402).json(`Product has less than ${totalUnits} units`)
+            return res.status(402).json({ error: `Product has less than ${totalUnits} units` })
         }
 
         if(productAlreadyInCart) {
             console.log(productAlreadyInCart)
             const newAmount = productAlreadyInCart.total_units + totalUnits;
             if(newAmount < 1) {
-                return res.status(403).json(`Product total in the cart can't be zero. Do you want to delete this product from cart?`);
+                return res.status(403).json({ error: `Product total in the cart can't be zero. Do you want to delete this product from cart?` });
             }
             if (newAmount > totalProductQty) {
-                return res.status(404).json(`Product does not have that many units`);
+                return res.status(404).json({ error: `Product does not have that many units` });
             }
             const updateCart = await Database.updateProductQuanityInCart(userId, productId, newAmount);
-            return res.status(200).json(`Product ${productId} quantity has been udpated in the cart`);
+            return res.status(200).json({ message: `Product ${productId} quantity has been udpated in the cart` });
         } else {
             if(totalUnits < 1) {
-                return res.status(405).json(`Total units for product needs to be greater than zero to be added to cart`);
+                return res.status(405).json({ error: `Total units for product needs to be greater than zero to be added to cart` });
             }
             const addToCartTable = await Database.addProductToCart(userId, productId, totalUnits);
-            return res.status(201).json(`Product ${productId} was added to cart table`);
+            return res.status(201).json({ message: `Product ${productId} was added to cart table` });
         }
     }
 });
@@ -141,35 +141,30 @@ router.post('/', async (req, res, next) => {
 router.get('/', async (req, res, next) => {
     // check if user is not logged in
     if(!req.session.passport) {
-        return res.status(500).json({msg: `User is not logged in`});    
-    } else {
-        const userId = req.session.passport.user.userId;
-        const validUserId = await Database.selectUserById(userId);
-        
-        if(!validUserId) {
-            return res.status(400).json(`User was not found`);
-        }
-        
-        const userCartData = await Database.selectCartProducts(userId);
-        
-        return res.status(200).json({msg: `Cart selected for user`, cart: userCartData});
+        return res.status(500).json({ error: `User is not logged in` });
+    } 
+
+    const userId = req.session.passport.user.userId;
+    const validUserId = await Database.selectUserById(userId);
+    if(!validUserId) {
+        return res.status(400).json({ error: `User was not found`});
     }
+    
+    const userCartData = await Database.selectCartProducts(userId);
+    
+    return res.status(200).json({ message: `Cart selected for user`, cart: userCartData });
 });
 
 /**
  * @swagger
- * /cart/:userId/:productId:
+ * /cart/delete-product:
  *      delete:
  *          tags:
  *              - Cart
  *          description: Delete product from cart
  *          parameters:
- *              - name: userId
- *                in: path
- *                description: Get user id
- *                required: true
  *              - name: productId
- *                in: path
+ *                in: query
  *                description: Get product id
  *                required: true
  *          responses:
@@ -185,79 +180,80 @@ router.get('/', async (req, res, next) => {
  *                  description: User is not logged in
  */
 
-router.delete('/:userId/:productId', async (req, res, next) => {
-    // check if user is not logged in
-    if(!req.session.passport) {
-        return res.status(500).json({msg: `User is not logged in`});
-    } else {
-        const userId = Number(req.params.userId);
-        const validUserId = await Database.selectUserById(userId);
-        if(!validUserId) {
-            return res.status(400).json(`User id was not found`);
-        }
-        
-        const productId = Number(req.params.productId);
-        const validProductId = await Database.checkIfProductAlreadyExists(productId);
-        if(!validProductId) {
-            return res.status(401).json(`Product id was not found`);
-        }
-
-        const productAlreadyInCart = await Database.selectProductInCart(userId, productId);
-        if(!productAlreadyInCart) {
-            return res.status(402).json(`No products in the cart`);
-        }
-        
-        const deleteProductFromCart = await Database.deleteProductFromCart(userId, productId)
-        return res.status(200).json(`Product was deleted from user userId cart`)
+router.delete('/delete-product', async (req, res, next) => {
+    // Check if productId query param is not provided  
+    if (!req.query.productId) {
+      return res.status(402).json({ error: 'ProductId parameter was not provided' });
     }
-});
+  
+    // Check if user is not logged in
+    if (!req.session.passport) {
+      return res.status(500).json({ error: 'User is not logged in' });
+    }
+  
+    const userId = req.session.passport.user.userId;
+  
+    // Check if the user ID is valid
+    const validUserId = await Database.selectUserById(userId);
+    if (!validUserId) {
+      return res.status(400).json({ error: 'User id was not found' });
+    }
+  
+    // Check if the product ID is valid
+    const productId = Number(req.query.productId);
+    const validProductId = await Database.checkIfProductAlreadyExists(productId);
+  
+    if (!validProductId) {
+      return res.status(404).json({ error: 'Product id was not found' });
+    }
+  
+    // Check if the product is already in the cart
+    const productAlreadyInCart = await Database.selectProductInCart(userId, productId);
+    if (!productAlreadyInCart) {
+      return res.status(402).json({ error: 'No products in the cart' });
+    }
+  
+    // Delete the product from the cart
+    const deleteProductFromCart = await Database.deleteProductFromCart(userId, productId);
+  
+    return res.status(200).json({ message: 'Product was deleted from user cart' });
+  });  
 
 /**
  * @swagger
- * /cart/:userId/:
+ * /cart/delete-cart:
  *      delete:
  *          tags:
  *              - Cart
  *          description: Delete user's cart
- *          parameters:
- *              - name: userId
- *                in: path
- *                description: Get user id
- *                required: true
  *          responses:
  *              200:
  *                  description: All products deleted from user's' cart
  *              400:
  *                  description: User id was not found
  *              401:
- *                  description: Product id was not found
- *              402:
  *                  description: No products in the cart
  *              500:
  *                  description: User is not logged in
  */
 
-router.delete('/:userId', async (req, res, next) => {
+router.delete('/delete-cart', async (req, res, next) => {
     // check if user is not logged in
     if(!req.session.passport) {
-        return res.status(500).json({msg: `User is not logged in`});
+        return res.status(500).json({ error: `User is not logged in` });
     } else {
+        const userId = req.session.passport.user.userId;
         const validUserId = await Database.selectUserById(userId);
-        if(!validUserId && userId !== 0) {
-            return res.status(400).json(`User id was not found`);
-        }
-        
-        const userId = Number(req.params.userId);
         if(!validUserId) {
-            return res.status(402).json(`User id ${userId} was not found`);
+            return res.status(400).json({ error: `User id was not found` });
         }
         
         const productsInUserCart = await Database.selectCartProducts(userId);
         if(productsInUserCart.length === 0) {
-            return res.status(403).json(`No products in the user ${userId} cart`);
+            return res.status(401).json({ error: `No products in the cart` });
         }
         const deleteProductFromCart = await Database.deleteAllProductsFromCart(userId);
-        return res.status(200).json({msg: `All products deleted from user cart`, cart: productsInUserCart});
+        return res.status(200).json({ message: `All products deleted from user cart` });
     }
 });
 
