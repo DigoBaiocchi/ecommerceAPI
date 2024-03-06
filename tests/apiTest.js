7caf0d65e11cb4ce54a3b59a7f46391e82646e96
Rodrigo <digo.baiocchi@gmail.com>
2024-03-05 20:03:16 -0500

    Adding steps for adding all the tests in apiTest file

--- tests/apiTest.js
+++ tests/apiTest.js
@@ -6,6 +6,20 @@ require('./cartApiTest');
 require('./categoriesApiTest');
 require('./productsApiTest');
 
+// Register user
+// Add user info
+// Add category
+// Add product
+// Add product to cart
+// Add data from cart to checkout
+// Create order from checkout data
+// Delete order
+// Delete cart
+// Delete product
+// Delete category
+// Delete user info
+// Delete user
+
 after((done) => {
     server.close(err => {
         if (err) return done(err);
