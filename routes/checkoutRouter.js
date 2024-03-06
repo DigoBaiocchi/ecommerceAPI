bdef8b07d9d8e8721ee798ae57d5471bd9e7020c
Rodrigo <digo.baiocchi@gmail.com>
2024-03-05 21:13:03 -0500

    Log user in checkout tests and userNotLogged middleware
    Logging user in checkout tests and passing login cookie to tests.
    Adding userNotLoggerInError middleware to checkout router and adding 402
    User has no products in cart error response

--- routes/checkoutRouter.js
+++ routes/checkoutRouter.js
@@ -2,6 +2,16 @@ const express = require('express');
 const router = express.Router();
 const { Database } = require('../db/databaseQueries');
 
+// Router middlewares
+const userNotLoggedInError = (req, res, next) => {
+    if(!req.session.passport) {
+        return res.status(401).json({ error: `User is not logged in` });
+    } else {
+        req.userId = req.session.passport.user.userId;
+        next();
+    }
+};
+
 /**
  * @swagger
  * components:
@@ -33,17 +43,17 @@ const { Database } = require('../db/databaseQueries');
  *                  description: User has no products in the cart
  *                  schema:
  *                      $ref: '#definitions/Cart'
+ *              401:
+ *                  description: User is not logged in
  */
 
-router.get('/', async (req, res, next) => {
-    const { userId } = req.body;
-    const validUserId = await Database.selectUserById(userId);
-    
-    if(!validUserId) {
+router.get('/', userNotLoggedInError, async (req, res, next) => {    
+    const cartData = await Database.getProductInfoWithPriceFromCart(req.userId);
+    if(cartData.length === 0) {
         return res.status(400).json({ error: `User has no products in the cart` })
     }
-    
-    const cartData = await Database.getProductInfoWithPriceFromCart(userId);
+
+
     return res.status(200).json({ message: `All products from user's cart are loaded`, data: cartData });
 });
 
@@ -66,39 +76,32 @@ router.get('/', async (req, res, next) => {
  *          responses:
  *              201:
  *                  description: Order successfully created
- *              400:
- *                  description: User was not found
  *              401:
  *                  description: User is not logged in
  *              402:
  *                  description: User has no products in their cart
  */
 
-router.post('/', async (req, res, next) => {
+router.post('/', userNotLoggedInError, async (req, res, next) => {
     const lastOrderNumber = await Database.checkLastOrderNumber();
 
-    if(!req.session.passport) {
-        return res.status(401).json({ error: `User is not logged in` });
-    }
-
-    const userId = req.session.passport.user.userId;
-    const validUserId = await Database.selectUserById(userId);
-
-    if (!validUserId) {
-        return res.status(400).json({ error: `User was not found` });
-    }
-
-    const { checkoutData } = req.body
-
+    const { checkoutData } = req.body;
     if(checkoutData.length === 0) {
         return res.status(402).json({ error: `User has no products in their cart` });
     }
 
     checkoutData.forEach(async cart => {
-        const createOrder = await Database.createOrder(lastOrderNumber + 1, cart.user_id, cart.product_id, cart.total_units, cart.price, 'Pending');
+        const createOrder = await Database.createOrder(
+            lastOrderNumber + 1, 
+            req.userId, 
+            cart.productId, 
+            cart.totalUnits, 
+            cart.price, 
+            'Pending'
+        );
     });
 
-    const clearUserCart = await Database.deleteAllProductsFromCart(userId);
+    const clearUserCart = await Database.deleteAllProductsFromCart(req.userId);
 
     return res.status(201).json({ message: `Order successfully created` });
 });
--- tests/checkoutApiTest.js
+++ tests/checkoutApiTest.js
@@ -1,5 +1,8 @@
 const request = require('supertest');
 const { app } = require('../app');
+const { newUserData } = require('./mockData/mockData');
+
+let cookie;
 
 const data = {
     "userId": 6,
@@ -15,6 +18,63 @@ const wrongUserIdData = {
     "price": '$14.20'
 };
 
+before((done) => {
+    // login user
+    request(app)
+        .post('/auth/login/')
+        .send(newUserData)
+        .end((err, res) => {
+            if (err) return done(err);
+            cookie = res.headers['set-cookie'];
+            done();
+        })
+});
+
+describe('POST /checkout', () => {
+    const path = '/checkout';
+    it('responses with 201 when order is created from checkout and products are cleared from cart', (done) => {
+        request(app)
+        .post(path)
+        .set('Cookie', cookie)
+        .send({'checkoutData': [data]})
+        .set('accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(201)
+        .expect({ "message": `Order successfully created` })
+        .end((err) => {
+            if (err) return done(err);
+            done();
+        })
+    });
+    it('responses with 402 when no user has no products in their cart', (done) => {
+        request(app)
+            .post(path)
+            .set('Cookie', cookie)
+            .send({'checkoutData': []})
+            .set('accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(402)
+            .expect({ "error": `User has no products in their cart` })
+            .end((err) => {
+                if (err) return done(err);
+                done();
+            })
+        });
+    it('responses with 401 when user is not logged in', (done) => {
+        request(app)
+        .post(path)
+        .send({'checkoutData': [data]})
+        .set('accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(401)
+        .expect({ "error": `User is not logged in` })
+        .end((err) => {
+            if (err) return done(err);
+            done();
+        })
+    });
+});
+
 describe('GET /checkout', () => {
     const path = '/checkout';
     const goodUserIdData = {'userId': data['userId']};
@@ -22,7 +82,8 @@ describe('GET /checkout', () => {
     it('responses with 200 with all products in checkout', (done) => {
         request(app)
             .get(path)
-            .send(goodUserIdData)
+            .set('Cookie', cookie)
+            // .send(goodUserIdData)
             .set('accept', 'application/json')
             .expect('Content-Type', /json/)
             .expect(200)
@@ -35,7 +96,8 @@ describe('GET /checkout', () => {
     it('responses with 400 when no products are found for user', (done) => {
         request(app)
             .get(path)
-            .send(badUserIdData)
+            .set('Cookie', cookie)
+            // .send(badUserIdData)
             .set('accept', 'application/json')
             .expect('Content-Type', /json/)
             .expect(400)
@@ -45,40 +107,10 @@ describe('GET /checkout', () => {
                 done();
             })
     });
-});
-
-describe('POST /checkout', () => {
-    const path = '/checkout';
-    it('responses with 201 when order is created from checkout and products are cleared from cart', (done) => {
-        request(app)
-            .post(path)
-            .send(data)
-            .set('accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(201)
-            .expect({ "message": `Order successfully created` })
-            .end((err) => {
-                if (err) return done(err);
-                done();
-            })
-    });
-    it('responses with 400 when no user id was found', (done) => {
-        request(app)
-            .post(path)
-            .send(wrongUserIdData)
-            .set('accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(400)
-            .expect({ "error": `User was not found` })
-            .end((err) => {
-                if (err) return done(err);
-                done();
-            })
-    });
     it('responses with 401 when user is not logged in', (done) => {
         request(app)
             .post(path)
-            .send(wrongUserIdData)
+            .send(goodUserIdData)
             .set('accept', 'application/json')
             .expect('Content-Type', /json/)
             .expect(401)
