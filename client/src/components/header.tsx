cab64256e9a4bfb3db2c6e2adc4b5b926ec78182
Rodrigo <digo.baiocchi@gmail.com>
2024-03-14 21:05:34 -0400

    creating Header component and add to App.tsx

---
+++ client/src/components/header.tsx
@@ -0,0 +1,15 @@
+function Header() {
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Products</li>
+                    <li>Cart</li>
+                </ul>
+            </nav>
+        </>
+    )
+}
+
+export default Header;
