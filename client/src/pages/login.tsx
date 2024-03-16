38cb43dbdde57462be663c264aad5d3e188c3d6a
Rodrigo <digo.baiocchi@gmail.com>
2024-03-15 21:54:16 -0400

    Log in user email and password
    Logging user in and navigate to '/' if email and password matches with
    database, adding POST fetch to check credentials

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,12 +1,48 @@
+import { SetStateAction, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 function Login() {
+    const navigate = useNavigate();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const onChangeEmail = (e: { target: { value: SetStateAction<string>; }; }) => {
+        setEmail(e.target.value);
+    };
+    const onChangePassword = (e: { target: { value: SetStateAction<string>; }; }) => {
+        setPassword(e.target.value);
+    };
+
+    const onSubmit = async (event: { preventDefault: () => void; }) => {
+        event.preventDefault();
+        const tryLogin = await fetch('http://localhost:3001/auth/login', {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify({email, password, checkoutData: []})
+        });
+        
+        const response = await tryLogin.json();
+        if (response.message === 'User is logged in') {
+            navigate('/');
+        }
+    };
+    
+    useEffect(() => {
+        console.log(email);
+        console.log(password);    
+    }, [email, password]);
+
     return (
         <>
-            <form action="">
+            <form onSubmit={onSubmit}>
                 <label htmlFor="email">Email:</label>
-                <input type="text" id="email" placeholder="Email" />
+                <input type="text" id="email" placeholder="Email" onChange={onChangeEmail} />
                 <label htmlFor="password">Password:</label>
-                <input type="text" id="password" placeholder="Password" />
-                <button>Login</button>
+                <input type="text" id="password" placeholder="Password" onChange={onChangePassword} />
+                <button type="submit">Login</button>
             </form>
         </>
     );
