18890c68bc5c5fb0fbf74771238fb4eb95ec6cc7
Rodrigo <digo.baiocchi@gmail.com>
2024-03-17 21:21:42 -0400

    creating reducers to set user email, password, username and cart. Thunk middleware created to login user and set username

--- client/src/store/userSlice.tsx
+++ client/src/store/userSlice.tsx
@@ -1,25 +1,55 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { login } from "../api/api";
 
 type user = {
     email:String,
     username:String,
     password:String,
+    cart:Array<object>
 };
 
 const initialState:user = {
     email: '',
     username: '',
     password: '',
+    cart: [],
 };
 
 export const userSlice = createSlice({
     name: 'userData',
     initialState,
     reducers: {
-        setUserData: state => {}
+        setUserEmail: (state, action) => {
+            state.email = action.payload
+        },
+        setUserUsername: (state, action) => {
+            state.username = action.payload.username
+        },
+        setUserPassword: (state, action) => {
+            state.password = action.payload
+        },
+        setUserCart: (state, action) => {
+            state.cart = action.payload
+        }
     }
 });
 
-export const { setUserData } = userSlice.actions;
+export const { 
+    setUserEmail,
+    setUserUsername,
+    setUserPassword,
+    setUserCart,
+} = userSlice.actions;
 
 export default userSlice.reducer;
+
+// ThunkMiddleware
+export const loginUser = () => async (dispatch: any, getState: any) => {
+    try {
+        const userData = await login(getState.email, getState.password, getState.cart);
+    
+        dispatch(setUserUsername(userData.response));
+    } catch (err) {
+        console.log(err);
+    }
+}
