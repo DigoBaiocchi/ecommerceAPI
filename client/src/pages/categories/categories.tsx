e8a0b4ac2ed0307b1e3d3b9af4e7500b5d6d206f
Rodrigo <digo.baiocchi@gmail.com>
2024-04-06 21:04:04 -0400

    adding categoryTableBody component to Categories page

--- client/src/pages/categories/categories.tsx
+++ client/src/pages/categories/categories.tsx
@@ -1,24 +1,15 @@
 import { useDispatch, useSelector } from "react-redux";
 import Header from "../../components/header/header";
 import { AppDispatch } from "../../store/store";
-import { addCategory, deleteCategory, getCategories, selectCategories, updateCategory } from "../../store/categoriesSlice";
+import { addCategory, getCategories, selectCategories } from "../../store/categoriesSlice";
 import { SetStateAction, useEffect, useState } from "react";
-
-type EditedCategory = {
-    name:string,
-    hideMessage:boolean,
-    updateButtonMessage:string,
-    inputDisabled:boolean
-};
+import CategoryTableBody from "../../components/categoryTableBody/categoryTableBody";
 
 function Categories() {
     const dispatch:AppDispatch = useDispatch();
     const categories = useSelector(selectCategories);
     const [categoryName, setCategoryName] = useState('');
     const [triggerRefetch, setTriggerRefetch] = useState(false);
-    const [inputDisabled, setInputDisabled] = useState(true);
-    const [editedCategory, setEditedCategory] = useState<{ [key: number]: EditedCategory }>({});
-    const [editButtonName, setEditButtonName] = useState('Update Name');
 
     const onChangeCategoryname = (e: { target: { value: SetStateAction<string>; }; }) => {
         setCategoryName(e.target.value);
@@ -28,52 +19,9 @@ function Categories() {
         event.preventDefault();
         dispatch(addCategory(categoryName)).then(() => {
             setTriggerRefetch(true);
+            setCategoryName('');
         });
     };
-    
-    const onClickDelete = (categoryId: number): React.MouseEventHandler<HTMLButtonElement> => () => {
-        dispatch(deleteCategory(categoryId)).then(() => {
-            setTriggerRefetch(true);
-        });;
-    };
-    
-    const onClickUpdate = (categoryId: number): React.MouseEventHandler<HTMLButtonElement> => () => {
-        setInputDisabled(!inputDisabled);
-        if (editButtonName === 'Submit Change') {
-            dispatch(updateCategory(categoryId, editedCategory[categoryId].name)).then((response) => {
-                console.log(response)
-            });
-            setEditedCategory((prev) => ({
-                ...prev,
-                [categoryId]: {
-                    ...prev[categoryId],
-                    updateButtonMessage: 'Update Name'
-                }
-            }));
-        } else {
-            setEditedCategory((prev) => ({
-                ...prev,
-                [categoryId]: {
-                    ...prev[categoryId],
-                    updateButtonMessage: 'Submit Change'
-                }
-            }));
-        }
-        console.log(editedCategory);
-    };
-
-    const handleChangeName = (categoryId:number) => (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { value } = e.target;
-        setEditedCategory((prev) => ({
-            ...prev,
-            [categoryId]: {
-                name: value, 
-                hideMessage: value === '' ? false : true,
-                updateButtonMessage: 'Submit Change',
-                inputDisabled: false
-            }
-        }));
-    }
     
     useEffect(() => {
         dispatch(getCategories());
@@ -84,10 +32,11 @@ function Categories() {
         <>
             <Header />
             <form onSubmit={onSubmit}>
-                <label htmlFor="category-name">Category Name</label>
+                <label htmlFor="category-name">Category Name:</label>
                 <input 
                     type="text" 
                     id="category-name" 
+                    value={categoryName}
                     onChange={onChangeCategoryname}
                 />
                 <button type="submit">Add Category</button>
@@ -105,32 +54,7 @@ function Categories() {
                 <tbody>
                     {
                         categories.map(category => (
-                            <tr key={category.id}>
-                                <td>{category.id}</td>
-                                <td>
-                                    <input 
-                                        type="text" 
-                                        value={editedCategory[category.id] !== undefined ? 
-                                            editedCategory[category.id].name : 
-                                            category.name} 
-                                        disabled={inputDisabled} 
-                                        onChange={handleChangeName(category.id)}
-                                    />
-                                </td>
-                                <button onClick={onClickDelete(category.id)}>Delete</button>
-                                <button 
-                                    onClick={onClickUpdate(category.id)} 
-                                    value={editButtonName}>
-                                        {editedCategory[category.id] !== undefined ? 
-                                            editedCategory[category.id].updateButtonMessage : 
-                                            'Update Name'}
-                                </button>
-                                <span 
-                                    hidden={editedCategory[category.id] !== undefined ? 
-                                        editedCategory[category.id].hideMessage :
-                                        true}
-                                    >Category Name can't be blank!</span>
-                            </tr>
+                            <CategoryTableBody id={category.id} name={category.name} />
                         ))
                     }
                 </tbody>
