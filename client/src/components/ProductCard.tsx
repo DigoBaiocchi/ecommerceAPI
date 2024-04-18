2872e1bba14b6d6be49aef6decf07e0113261d7c
Rodrigo <digo.baiocchi@gmail.com>
2024-04-17 21:42:43 -0400

    creating ProductCard component and adding to Home page

---
+++ client/src/components/ProductCard.tsx
@@ -0,0 +1,13 @@
+export default function ProductCard() {
+    return (
+        <div>
+            <div>
+                <img src="" alt="" />
+            </div>
+            <div>
+                <span>Product A</span>
+                <span>$5.99</span>
+            </div>
+        </div>
+    );
+}
--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,10 +1,13 @@
+import ProductCard from "../../components/ProductCard";
 import Header from "../../components/header/header";
 
 export default function Home() {
     return (
         <>
             <Header />
-            
+            <main>
+                <ProductCard />
+            </main>
         </>
     );
 }
